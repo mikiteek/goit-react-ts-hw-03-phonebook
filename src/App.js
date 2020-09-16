47a50f85e49867ae86eaac7c430490ca4d3205c7
mikiteek <mikiteek@gmail.com>
2020-09-16 22:34:35 +0300

    Implement able to filter contacts by name

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import {v4 as uuid} from "uuid";
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
     name: "",
     number: "",
+    filter: "",
   };
+
   addContact = contact => {
     const {name, number} = contact;
     if (name === "" || number === "")
@@ -20,6 +27,7 @@ class App extends Component {
       contacts: [...contacts, contactNew],
     }));
   };
+
   handleSubmit = event => {
     event.preventDefault();
     const contact = {
@@ -28,13 +36,22 @@ class App extends Component {
     }
     this.addContact(contact);
   };
+
   handleChange = event => {
     const {name, value} = event.target;
     this.setState({[name]: value});
   };
 
+  getVisibleContacts = () => {
+    const {filter, contacts} = this.state;
+    return contacts.filter(({name}) => name.toLowerCase().includes(filter.toLowerCase()));
+  }
+  changeFilter = event => {
+    this.setState({filter: event.target.value});
+  }
+
   render() {
-    const contacts = this.state.contacts;
+    const contacts = this.getVisibleContacts();
     return (
       <>
         <section>
@@ -50,6 +67,10 @@ class App extends Component {
         </section>
         <section>
           <h2>Contacts</h2>
+          <label>Find contacts by name
+            <br/>
+            <input type="text" name="filter" onChange={this.changeFilter}/>
+          </label>
           <ul>
             {contacts.length > 0 && contacts.map(({name, number, id}) => (
               <li key={id}>{name}: {number}</li>
